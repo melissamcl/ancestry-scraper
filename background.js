b35ff479b5c0bc4762cfe3aa2aaeadf0b086bb7f
melissamcl <mcl.melissa.m@gmail.com>
2024-02-29 20:17:32 -0500

    one batch processing, recursion not yet implemented

--- background.js
+++ background.js
@@ -22,60 +22,17 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-// Once URL changes and page is loaded, send message to scrape matches
-// function setupUrlUpdateListener() {
-//   let messageSent = false;
-
+// once URL changes and page is loaded, send message to scrape matches
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  // if (changeInfo.status === 'complete' && !messageSent) {
   if (changeInfo.status === 'complete') {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      if (tab.url.indexOf('?') > -1) {
+      // only send if there are min/max and before scrolling
+      if (tab && tab.url && tab.url.indexOf('?') > -1) {
         chrome.tabs.sendMessage(tabId, { action: 'scrapeMatches' });
         console.log('url updated, scrape matches message sent');
-        // messageSent = true;
+        incrementCounter();
+        console.log(`Counter value: ${counter}`);
       }
     });
   }
-  // else {
-  //   messageSent = false;
-  // }
 });
-// }
-// setupUrlUpdateListener();
-
-// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-//   if (message.message === 'updateUrl') {
-//     const { minCm, maxCm, url } = message;
-//     const newUrl = `${url}?minshareddna=${minCm}&maxshareddna=${maxCm}`;
-
-//     console.log('new url', newUrl);
-
-//     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-//       const currentTab = tabs[0];
-//       chrome.tabs.update(currentTab.id, { url: newUrl }, () => {
-//         console.log('URL updated');
-//       });
-
-//       // Listen for the tab to finish loading
-//       chrome.tabs.onUpdated.addListener(function listener(
-//         tabId,
-//         changeInfo,
-//         updatedTab
-//       ) {
-//         if (tabId === currentTab.id && changeInfo.status === 'complete') {
-//           // The tab has finished loading, so we can now send the response
-//           console.log('Page reloaded with new URL');
-//           sendResponse({ success: true });
-//           console.log('response sent');
-
-//           // Remove the listener to avoid multiple executions
-//           chrome.tabs.onUpdated.removeListener(listener);
-//         }
-//       });
-//     });
-
-//     // Return true to indicate that sendResponse will be called asynchronously
-//     return true;
-//   }
-// });
--- scripts/scrapeMatches.js
+++ scripts/scrapeMatches.js
@@ -1,157 +1,143 @@
-// TODO: Set shared DNA filter
+function updateUrl(minCmInput) {
+  const url = window.location.href;
 
-// function scrapeMatches(minCmInput) {
-//   console.log('scrapeMatches start');
-//   const matchListUrl = window.location.href;
+  // extract parameters from url if list is already filtered
+  const matchListUrl = url.split('?')[0];
+  const urlParams = new URLSearchParams(url.split('?')[1]);
+  const minCmApplied = urlParams.get('minshareddna');
+  const maxCmApplied = urlParams.get('maxshareddna');
 
-//   // const allMatches = [];
-//   // const currBatch = [];
+  // stop recursion at the max of 6 or minCmInput
+  const stopAtCm = Math.max(6, minCmInput);
 
-//   if (
-//     matchListUrl.startsWith(
-//       'https://www.ancestry.com/discoveryui-matches/list/'
-//     )
-//   ) {
-//     filterMatches(minCmInput);
-//   }
+  // update parameters accordingly
+  // initialize parameters for first filter
+  if (!minCmApplied && !maxCmApplied) {
+    newMinCm = Math.max(minCmInput, 30);
+    newMaxCm = 3490;
+  }
+  // if max of minCmApplied/maxCmApplied <= stopAtCm, stop
+  // maxCm may be null when the max is really 3490
+  else if (Math.max(minCmApplied, maxCmApplied) <= stopAtCm) {
+    return;
+  }
+  // This batching logic attempts to load 500-1000 matches at a time
+  // if current min is > 20, reduce new min by 5, but don't go below minCmInput
+  else if (minCmApplied > 20) {
+    newMaxCm = minCmApplied - 1;
+    newMinCm = Math.max(minCmInput, minCmApplied - 5);
+  }
+  // if current min is 20 or below, reduce by 1cm at a time
+  else if (minCmApplied <= 20) {
+    newMinCm = minCmApplied - 1;
+    newMaxCm = newMinCm;
+  }
 
-//   function filterMatches(minCmInput, minCm = 30, maxCm = 3490) {
-//     console.log('filter matches minCm:', minCmInput);
+  // send message to bg to update url
+  chrome.runtime.sendMessage({
+    action: 'updateUrl',
+    url: matchListUrl,
+    minCm: newMinCm,
+    maxCm: newMaxCm,
+    minCmInput: minCmInput,
+  });
+}
 
-//     // base case: once minCm <= to min input, stop
-//     if (maxCm < minCmInput) {
-//       // process match list array
-//       console.log('matches processed');
-//     }
+function scrapeMatches() {
+  console.log('Message received to scrape matches');
 
-//     // recursive case: reduce max/min and repeat
-//     else if (maxCm >= minCmInput) {
-//       minCm = Math.max(minCm, minCmInput);
-//       console.log('recursive case');
+  function loadMatches() {
+    const matches = document.getElementsByClassName('matchGrid');
+    console.log('matches count:', matches.length);
+    if (matches.length >= 100) {
+      console.log('Matches loaded');
+      return true;
+    }
 
-//       // chrome.runtime.sendMessage(
-//       //   {
-//       //     message: 'updateUrl',
-//       //     url: matchListUrl,
-//       //     minCm: minCm,
-//       //     maxCm: maxCm,
-//       //   },
-//       //   (response) => {
-//       //     // console.log('response', response);
-//       //     console.log('loading matches');
-//       //   }
-//       // );
+    setTimeout(function () {
+      loadMatches();
+    }, 1000);
+  }
 
-//       console.log('loading matches');
+  function scrollToBottom(callback) {
+    let lastHeight = 0;
+    let count = 0;
+    const maxCount = 3; // Max attempts before determining end of page has been reached
+    const interval = setInterval(() => {
+      window.scrollTo(0, document.body.scrollHeight);
+      const newHeight = document.body.scrollHeight;
+      if (newHeight === lastHeight) {
+        count++;
+        if (count >= maxCount) {
+          clearInterval(interval);
+          callback();
+        }
+      } else {
+        lastHeight = newHeight;
+        count = 0; // Reset count
+      }
+    }, 1000); // Adjust the interval as needed
+  }
 
-//       // scrollToBottom(() => {
-//       //   console.log('Reached bottom of page');
-//       //   checkForMatchEntries();
-//       // });
+  function getMatches() {
+    const matches = document.querySelectorAll('.matchGrid');
+    const matchInfo = [];
 
-//       // // recursively call filterMatches
-//       // let newMinCm;
-//       // let newMaxCm;
-//       // if (minCm > 20) {
-//       //   newMinCm = minCm - 5;
-//       //   newMaxCm = newMinCm + 4;
-//       // } else {
-//       //   newMinCm = minCm - 1;
-//       //   newMaxCm = newMinCm;
-//       // }
+    if (matches.length > 0) {
+      console.log('Match entries found:', Array.from(matches));
+    } else {
+      console.log('No match entries found');
+    }
 
-//       // filterMatches(minCmInput, newMinCm, newMaxCm);
-//     }
-//   }
+    for (const match of matches) {
+      const matchId = match.id.substring(5);
+      const fullName = match.childNodes[0].innerText.split('\n');
+      const name = fullName[0];
+      let manager = '';
+      if (fullName[1]) {
+        manager = fullName[1].replace('Managed by ', '');
+      }
+      const relationship = match.childNodes[3].childNodes[0].innerText;
+      const sharedDNA =
+        match.childNodes[3].childNodes[2].innerText.split(' cM | ');
+      const sharedCm = sharedDNA[0].replace(',', '');
+      const sharedPercent = sharedDNA[1].replace('% shared DNA', '');
+      const side = match.childNodes[3].childNodes[5].innerText;
+      const trees = match.childNodes[4].children[0].innerText;
+      let commonAncestor = false;
+      if (match.childNodes[4].children[1]) {
+        commonAncestor = true;
+      }
+      const notes = match.childNodes[7].innerText;
+      let tags = [];
+      if (match.childNodes[8].childNodes[0].childNodes[1].children[0]) {
+        for (const child of match.childNodes[8].childNodes[0].childNodes[1]
+          .children) {
+          tags.push(child.ariaLabel.replace('In group ', ''));
+        }
+      }
 
-//   // Confirm page loaded by looking for a match element (traditional event listeners don't work)
-//   function loadMatches() {
-//     const matches = document.getElementsByClassName('matchGrid');
-//     console.log('matches count:', matches.length);
-//     if (matches.length >= 50) {
-//       console.log('Matches loaded');
-//       return;
-//     }
+      matchInfo.push({
+        id: matchId,
+        name: name,
+        manager: manager,
+        relationship: relationship,
+        sharedCm: sharedCm,
+        sharedPercent: sharedPercent,
+        side: side,
+        trees: trees,
+        commonAncestor: commonAncestor,
+        notes: notes,
+        tags: tags,
+      });
+    }
 
-//     setTimeout(function () {
-//       loadMatches();
-//     }, 1000);
-//   }
+    console.log(matchInfo);
+  }
 
-//   function scrollToBottom(callback) {
-//     let lastHeight = 0;
-//     let count = 0;
-//     const maxCount = 3; // Max attempts before determining end of page has been reached
-//     const interval = setInterval(() => {
-//       window.scrollTo(0, document.body.scrollHeight);
-//       const newHeight = document.body.scrollHeight;
-//       if (newHeight === lastHeight) {
-//         count++;
-//         if (count >= maxCount) {
-//           clearInterval(interval);
-//           callback();
-//         }
-//       } else {
-//         lastHeight = newHeight;
-//         count = 0; // Reset count
-//       }
-//     }, 1000); // Adjust the interval as needed
-//   }
-
-//   function checkForMatchEntries() {
-//     const matchEntries = document.querySelectorAll('.matchGrid');
-//     if (matchEntries.length > 0) {
-//       console.log('Match entries found:', Array.from(matchEntries));
-//     } else {
-//       console.log('No match entries found');
-//     }
-
-//     for (const matchEntry of matchEntries) {
-//       const matchId = matchEntry.id.substring(5);
-//       const fullName = matchEntry.childNodes[0].innerText.split('\n');
-//       const name = fullName[0];
-//       let manager = '';
-//       if (fullName[1]) {
-//         manager = fullName[1].replace('Managed by ', '');
-//       }
-//       const relationship = matchEntry.childNodes[3].childNodes[0].innerText;
-//       const sharedDNA =
-//         matchEntry.childNodes[3].childNodes[2].innerText.split(' cM | ');
-//       const sharedCM = sharedDNA[0].replace(',', '');
-//       const sharedPercent = sharedDNA[1].replace('% shared DNA', '');
-//       const side = matchEntry.childNodes[3].childNodes[5].innerText;
-//       const trees = matchEntry.childNodes[4].children[0].innerText;
-//       let commonAncestor = false;
-//       if (matchEntry.childNodes[4].children[1]) {
-//         commonAncestor = true;
-//       }
-//       const notes = matchEntry.childNodes[7].innerText;
-//       let tags = [];
-//       if (matchEntry.childNodes[8].childNodes[0].childNodes[1].children[0]) {
-//         for (const child of matchEntry.childNodes[8].childNodes[0].childNodes[1]
-//           .children) {
-//           tags.push(child.ariaLabel.replace('In group ', ''));
-//         }
-//       }
-
-//       // if number of matches in previous batch is modular 100 redo it (just keep going without changing url?)
-//       //  in this case if the number still doesn't change after continuing assume it is correct
-
-//       console.log(
-//         `
-//       id: ${matchId},
-//       name: ${name},
-//       manager: ${manager},
-//       relationship: ${relationship},
-//       sharedCM: ${sharedCM},
-//       shared%: ${sharedPercent},
-//       side: ${side},
-//       trees: ${trees},
-//       commonAncestor: ${commonAncestor},
-//       notes: ${notes}
-//       tags: ${tags}
-//       `
-//       );
-//     }
-//   }
-// }
+  loadMatches();
+  scrollToBottom(() => {
+    console.log('Reached bottom of page');
+    getMatches();
+  });
+}
